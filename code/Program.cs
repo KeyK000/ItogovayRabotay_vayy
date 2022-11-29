11b314e41f4d5f24e8c9eb6100ef5dcc05a52d75
KEY <kirill.shoes@mail.ru>
2022-11-29 21:34:26 +0300

    Добавили процедуру фильтрации массива

--- code/Program.cs
+++ code/Program.cs
@@ -1,4 +1,18 @@
-﻿Console.WriteLine("Введите длинну массива: ");
+﻿void FilterArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+
+
+Console.WriteLine("Введите длинну массива: ");
 int count = Convert.ToInt32(Console.ReadLine());
 
 string[] defaultArray = new string[count];
@@ -9,3 +23,6 @@ for (int i = 0; i < defaultArray.Length; i++)
     defaultArray[i] = Console.ReadLine();
 }
 
+string[] filteredArray = new string[defaultArray.Length];
+
+FilterArray(defaultArray, filteredArray);
