aff4f422f367b65eb3f46f939994029bf5afafa1
KEY <kirill.shoes@mail.ru>
2022-11-29 21:19:49 +0300

    Добавлено заполнение массива с консоли

---
+++ code/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите длинну массива: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+string[] defaultArray = new string[count];
+
+Console.WriteLine("Заполни массив");
+for (int i = 0; i < defaultArray.Length; i++)
+{
+    defaultArray[i] = Console.ReadLine();
+}
+
